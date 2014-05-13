3d5ffc534de424f4e31ef225584b109f73361135
Jonathan Pidgeon <jon@empire5design.com>
2014-05-12 23:19:45 -0600

    asdf

--- client/defaults.js
+++
@@ -1,3 +0,0 @@
-Session.set('ircChannel', '#meteor');
-Session.set('ircLimit', '10');
-//todo: a better way without being coupled to these values
--- client/subscriptions.js
+++
@@ -1,5 +0,0 @@
-Deps.autorun(function() {
-  Meteor.subscribe('ircMessages', Session.get('ircChannel'), Session.get('ircLimit'));
-});
-Meteor.subscribe('ircChannels');
-Meteor.subscribe('ircConnections');
